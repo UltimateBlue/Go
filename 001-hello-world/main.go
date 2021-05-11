2e705a8ed06eab372b39f17a4d72ffc52ef96352
UltimateBlue <ultimatebluee@gmail.com>
2021-05-11 20:12:26 +0100

    add iteration and conditional

--- 001-hello-world/main.go
+++ 001-hello-world/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
+	fmt.Println("Peter Jan Singer")
+
+	foo()
+	fmt.Println("byeeee..")
+}
+
+func foo() {
+	for i := 1; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
 }
