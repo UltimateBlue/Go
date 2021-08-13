5a6cb511985fbfbe5b717c840adab146d8d869d4
UltimateBlue <ultimatebluee@gmail.com>
2021-08-13 10:22:38 +0330

    hello world

--- 001-hello-world/main.go
+++ 001-hello-world/main.go
@@ -3,16 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Peter Jan Singer")
+	fmt.Println("Hello world...")
 
-	foo()
-	fmt.Println("byeeee..")
-}
-
-func foo() {
-	for i := 1; i < 10; i++ {
-		if i%2 == 0 {
-			fmt.Println(i)
-		}
-	}
 }
